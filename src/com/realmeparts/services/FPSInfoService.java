aca4f9e40cf22b4a734ee1f6c8ca41b8cbf844d2
officialputuid <officialputuid@hack.id>
2022-03-08 18:17:45 +0800

    RealmeParts: FPSInfoService: Update WM. LayoutParams
    * TYPE_SYSTEM_OVERLAY -> TYPE_SECURE_SYSTEM_OVERLAY
    * Added FLAG_NOT_TOUCH_MODAL & FLAG_HARDWARE_ACCELERATED
    * Also fix can't allow permission after turn on FPSInfo
    
    Signed-off-by: officialputuid <officialputuid@hack.id>
    Change-Id: I0a659e32f680da8218f8db7bbe8a77852b884265

--- src/com/realmeparts/services/FPSInfoService.java
+++ src/com/realmeparts/services/FPSInfoService.java
@@ -92,9 +92,11 @@ public class FPSInfoService extends Service {
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                WindowManager.LayoutParams.TYPE_SECURE_SYSTEM_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
+                        WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
                 PixelFormat.TRANSLUCENT);
         params.gravity = Gravity.LEFT | Gravity.TOP;
         params.setTitle("FPS Info");
